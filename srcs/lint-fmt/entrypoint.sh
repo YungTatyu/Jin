4bc4fc13d293aaa92b0cd12b5ac6112112b8d851
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-09-20 00:46:52 +0900

    entrypoint.shでteraoさんのアドバイスに従い、修正。
    なお、backendとfrontendともにcompileのチェックする処理を追加しました。

--- srcs/lint-fmt/entrypoint.sh
+++ srcs/lint-fmt/entrypoint.sh
@@ -3,27 +3,69 @@
 
 set -e
 
-if [ "$1" = "fmt" ]; then
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+main() {
+  local cmd="$1"
+  if [[ ${cmd} != "fmt" && ${cmd} != "lint" ]]; then
+    err "unknown command"
+    return 1
+  fi
+  if [ "${cmd}" = "fmt" ]; then
     echo "Formatting frontend..."
-    #npm install --prefix $FRONTEND_DIR
-    npm run format --prefix $FRONTEND_DIR
+    npm run format --prefix ${FRONTEND_DIR}
 
     echo "Formatting backend..."
-    cargo fmt --manifest-path $TOML_FILE
+    cargo fmt --manifest-path ${TOML_FILE}
 
+  elif [ "${cmd}" = "lint" ]; then
+    echo "Linting backend..."
+    cargo clippy --manifest-path ${TOML_FILE} || return 1
+    echo "Compile backend..."
+    cargo check --manifest-path ${TOML_FILE} || return 1
 
-elif [ "$1" = "lint" ]; then
     echo "Linting frontend..."
-    npm run lint --prefix $FRONTEND_DIR
+    npm run lint --prefix ${FRONTEND_DIR} || return 1
 
     echo "Type-checking frontend..."
-    npm run type-check --prefix $FRONTEND_DIR
+    npm run type-check --prefix ${FRONTEND_DIR} || return 1
+
+    echo "Compile check..."
+    cd $FRONTEND_DIR
+    npx next build
+  fi
+  return 0
+}
+
+main "$@"
 
 
-    echo "Linting backend..."
-    cargo clippy --manifest-path $TOML_FILE
+# if [ "$1" = "fmt" ]; then
+#     echo "Formatting frontend..."
+#     #npm install --prefix $FRONTEND_DIR
+#     npm run format --prefix $FRONTEND_DIR
 
-else
-    echo "Unknown command: $1"
-    exit 1
-fi
+#     echo "Formatting backend..."
+#     cargo fmt --manifest-path $TOML_FILE
+
+
+# elif [ "$1" = "lint" ]; then
+#     echo "Linting frontend..."
+#     npm run lint --prefix $FRONTEND_DIR
+
+#     echo "Type-checking frontend..."
+#     npm run type-check --prefix $FRONTEND_DIR
+
+
+#     echo "Linting backend..."
+#     cargo clippy --manifest-path $TOML_FILE
+
+#     echo "Compile check..."
+#     cd $FRONTEND_DIR
+#     npx next build
+# else
+#     echo "Unknown command: $1"
+#     exit 1
+# fi
