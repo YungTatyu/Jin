dd16278dddced18664cebed582dd98d7f2179d0b
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-09-20 00:49:31 +0900

    コメントアウトを削除

--- srcs/lint-fmt/entrypoint.sh
+++ srcs/lint-fmt/entrypoint.sh
@@ -40,32 +40,3 @@ main() {
 }
 
 main "$@"
-
-
-# if [ "$1" = "fmt" ]; then
-#     echo "Formatting frontend..."
-#     #npm install --prefix $FRONTEND_DIR
-#     npm run format --prefix $FRONTEND_DIR
-
-#     echo "Formatting backend..."
-#     cargo fmt --manifest-path $TOML_FILE
-
-
-# elif [ "$1" = "lint" ]; then
-#     echo "Linting frontend..."
-#     npm run lint --prefix $FRONTEND_DIR
-
-#     echo "Type-checking frontend..."
-#     npm run type-check --prefix $FRONTEND_DIR
-
-
-#     echo "Linting backend..."
-#     cargo clippy --manifest-path $TOML_FILE
-
-#     echo "Compile check..."
-#     cd $FRONTEND_DIR
-#     npx next build
-# else
-#     echo "Unknown command: $1"
-#     exit 1
-# fi
