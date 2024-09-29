5eadc9f390c1cd68bf65c8f11100dae4bca0a3e2
rikeda-cloud <longsichitian648@gmail.com>
2024-09-29 13:14:32 +0900

    [update] テストケースのパラメータをUPPER_SNAKE_CASEにし、テストケース固有のパラメータを強調

--- srcs/app/backend/tests/refundable_escrow_test.ts
+++ srcs/app/backend/tests/refundable_escrow_test.ts
@@ -7,114 +7,114 @@ import { execCreateRefundableEscrow, execSettleLamports, ResultStatus } from "./
 import { sleep, getEscrowPDA, airdropLamports } from "./utils";
 
 describe("Refundable Escrow Program", () => {
-	const provider = anchor.AnchorProvider.env();
-	anchor.setProvider(provider);
+	const PROVIDER = anchor.AnchorProvider.env();
+	anchor.setProvider(PROVIDER);
 
 	const SECONDS = 5;
 	const SOL1 = (1 * LAMPORTS_PER_SOL); // 1SOL
 	const SOL2 = (2 * LAMPORTS_PER_SOL); // 2SOL
-	const amountLamports = new BN(SOL1);
-	const refundableSeconds = new BN(SECONDS);
-	const transactionId = new BN(1);
-	const userDefinedData = "TEST";
-	const program = anchor.workspace.RefundableEscrow;
-	const programId = program.programId;
+	const AMOUNT_LAMPORTS = new BN(SOL1);
+	const REFUNDABLE_SECONDS = new BN(SECONDS);
+	const TRANSACTION_ID = new BN(1);
+	const USER_DEFINED_DATA = "TEST";
+	const PROGRAM = anchor.workspace.RefundableEscrow;
+	const PROGRAM_ID = PROGRAM.programId;
 
 	it("should refund successfully", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
-		expect(await execSettleLamports(program, buyer, escrowPDA)).to.equal(ResultStatus.SUCCESS);
+		expect(await execSettleLamports(PROGRAM, buyer, escrowPDA)).to.equal(ResultStatus.SUCCESS);
 	});
 
 	it("should withdraw successfully", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
 		sleep(SECONDS + 2);
-		expect(await execSettleLamports(program, seller, escrowPDA)).to.equal(ResultStatus.SUCCESS);
+		expect(await execSettleLamports(PROGRAM, seller, escrowPDA)).to.equal(ResultStatus.SUCCESS);
 	});
 
 	it("should refund fail because it is outside refund period", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
 		sleep(SECONDS + 2);
-		expect(await execSettleLamports(program, buyer, escrowPDA)).to.equal(ResultStatus.ERROR);
+		expect(await execSettleLamports(PROGRAM, buyer, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should withdraw fail because it is inside refund period", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
-		expect(await execSettleLamports(program, seller, escrowPDA)).to.equal(ResultStatus.ERROR);
+		expect(await execSettleLamports(PROGRAM, seller, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should create PDA fail because amountLamports is negative", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
-		const negativeAmountLamports = -amountLamports;
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
+		const negativeAmountLamports = -AMOUNT_LAMPORTS;
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
+				TRANSACTION_ID,
 				negativeAmountLamports,
-				refundableSeconds,
-				userDefinedData
+				REFUNDABLE_SECONDS,
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
@@ -122,20 +122,20 @@ describe("Refundable Escrow Program", () => {
 	it("should create PDA fail because amountLamports is zero", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 		const zeroAmountLamports = new BN(0);
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
+				TRANSACTION_ID,
 				zeroAmountLamports,
-				refundableSeconds,
-				userDefinedData
+				REFUNDABLE_SECONDS,
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
@@ -143,19 +143,19 @@ describe("Refundable Escrow Program", () => {
 	it("should create PDA fail because buyer doesn't have enough amountLamports", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, (1 * LAMPORTS_PER_SOL) - 1);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, (1 * LAMPORTS_PER_SOL) - 1);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
-				amountLamports,
-				refundableSeconds,
-				userDefinedData
+				TRANSACTION_ID,
+				AMOUNT_LAMPORTS,
+				REFUNDABLE_SECONDS,
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
@@ -163,20 +163,20 @@ describe("Refundable Escrow Program", () => {
 	it("should create PDA fail because refundable seconds too long", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 		const tooLongRefundableSeconds = new BN((60 * 60 * 24 * 365) + 1);
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
-				amountLamports,
+				TRANSACTION_ID,
+				AMOUNT_LAMPORTS,
 				tooLongRefundableSeconds,
-				userDefinedData
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
@@ -184,20 +184,20 @@ describe("Refundable Escrow Program", () => {
 	it("should create PDA fail because refundable seconds too short", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 		const tooShortRefundableSeconds = new BN(0);
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
-				amountLamports,
+				TRANSACTION_ID,
+				AMOUNT_LAMPORTS,
 				tooShortRefundableSeconds,
-				userDefinedData
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
@@ -205,19 +205,19 @@ describe("Refundable Escrow Program", () => {
 	it("should create PDA fail because userDefinedData too large", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 		const tooLargeUserDefinedData = "A".repeat(101);
 
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
-				amountLamports,
-				refundableSeconds,
+				TRANSACTION_ID,
+				AMOUNT_LAMPORTS,
+				REFUNDABLE_SECONDS,
 				tooLargeUserDefinedData
 			)
 		).to.equal(ResultStatus.ERROR);
@@ -227,112 +227,112 @@ describe("Refundable Escrow Program", () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
 		const other = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		await airdropLamports(provider, other, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		await airdropLamports(PROVIDER, other, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
-		expect(await execSettleLamports(program, other, escrowPDA)).to.equal(ResultStatus.ERROR);
+		expect(await execSettleLamports(PROGRAM, other, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should withdraw fail because requestor is neigher the buyer nor seller", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
 		const other = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		await airdropLamports(provider, other, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		await airdropLamports(PROVIDER, other, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
 		sleep(SECONDS + 2);
-		expect(await execSettleLamports(program, other, escrowPDA)).to.equal(ResultStatus.ERROR);
+		expect(await execSettleLamports(PROGRAM, other, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should refund fail because refund was done multiple times", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
-		await execSettleLamports(program, buyer, escrowPDA);
-		expect(await execSettleLamports(program, buyer, escrowPDA)).to.equal(ResultStatus.ERROR);
+		await execSettleLamports(PROGRAM, buyer, escrowPDA);
+		expect(await execSettleLamports(PROGRAM, buyer, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should refund fail because withdraw was done multiple times", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
 		sleep(SECONDS + 2);
-		await execSettleLamports(program, seller, escrowPDA);
-		expect(await execSettleLamports(program, seller, escrowPDA)).to.equal(ResultStatus.ERROR);
+		await execSettleLamports(PROGRAM, seller, escrowPDA);
+		expect(await execSettleLamports(PROGRAM, seller, escrowPDA)).to.equal(ResultStatus.ERROR);
 	});
 
 	it("should create PDA fail because PDA already exists", async () => {
 		const seller = new Keypair();
 		const buyer = new Keypair();
-		await airdropLamports(provider, buyer, SOL2);
-		const escrowPDA = await getEscrowPDA(buyer, seller, transactionId, programId);
+		await airdropLamports(PROVIDER, buyer, SOL2);
+		const escrowPDA = await getEscrowPDA(buyer, seller, TRANSACTION_ID, PROGRAM_ID);
 
 		await execCreateRefundableEscrow(
-			program,
+			PROGRAM,
 			buyer,
 			seller,
 			escrowPDA,
-			transactionId,
-			amountLamports,
-			refundableSeconds,
-			userDefinedData
+			TRANSACTION_ID,
+			AMOUNT_LAMPORTS,
+			REFUNDABLE_SECONDS,
+			USER_DEFINED_DATA
 		);
 		expect(
 			await execCreateRefundableEscrow(
-				program,
+				PROGRAM,
 				buyer,
 				seller,
 				escrowPDA,
-				transactionId,
-				amountLamports,
-				refundableSeconds,
-				userDefinedData
+				TRANSACTION_ID,
+				AMOUNT_LAMPORTS,
+				REFUNDABLE_SECONDS,
+				USER_DEFINED_DATA
 			)
 		).to.equal(ResultStatus.ERROR);
 	});
