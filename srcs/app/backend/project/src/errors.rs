cd02245bc04dd3537c8e8ecd9c312d9fe5431c63
rikeda-cloud <longsichitian648@gmail.com>
2024-09-29 16:33:40 +0900

    [refactor] lib.rsファイルを複数ファイルに分割

---
+++ srcs/app/backend/project/src/errors.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Buyer doesn't hold the required lamports.")]
+    BuyerUnderfundedError,
+    #[msg("Invalid account provided.")]
+    InvalidAccountError,
+    #[msg("Refund period has expired.")]
+    RefundError,
+    #[msg("Withdrawals are not available during thr refund period.")]
+    FundraisingError,
+    #[msg("There are no funds available to withdraw.")]
+    CashShortageError,
+    #[msg("User defined data size is too large.")]
+    UserDefinedDataTooLarge,
+    #[msg("Refundable seconds is invalid.")]
+    RefundableSecondsError,
+    #[msg("Transaction amount is too small.")]
+    AmountTooSmall,
+}
