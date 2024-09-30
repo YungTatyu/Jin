d2be3f2fca4724f4dd82d2d3a86f738e97d90082
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-09-30 17:33:53 +0900

    校舎でテスト

--- srcs/app/build_tool/replace_program_id.sh
+++ srcs/app/build_tool/replace_program_id.sh
@@ -4,7 +4,7 @@ cargo build --manifest-path replace/Cargo.toml
 
 ./replace/target/debug/replace project/programs/project/src/lib.rs
 ./replace/target/debug/replace project/app/sample/test.js
-./replace/target/debug/replace project/app/components/AddNewTransaction/AddNewTransactionComponent.tsx
+./replace/target/debug/replace project/app/components/api.ts
 
 cargo clean --manifest-path replace/Cargo.toml
 
