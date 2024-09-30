46a37995ca26b67c522e606eddbca2cf211f2553
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-09-30 19:22:24 +0900

    /Users/yoshinarikusano/Developments/Jin/srcs/app/frontend/src/app/Providers/WalletProvider.tsx でlocalnetに変更。

--- srcs/app/frontend/src/app/Providers/WalletProvider.tsx
+++ srcs/app/frontend/src/app/Providers/WalletProvider.tsx
@@ -14,17 +14,22 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import '../styles/Providers/Wallet-adapter-override.css'; // 新しく追加
+import { Connection } from '@solana/web3.js';
 
 export default function WalletProviderWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Localnetのエンドポイントを指定
+  const endpoint = 'http://localhost:8899';
   const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  //const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  // コネクションオブジェクトの作成
+  const connection = useMemo(() => new Connection(endpoint), [endpoint]);
 
   const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    () => [new PhantomWalletAdapter({connection}), new SolflareWalletAdapter()],
     []
   );
 
