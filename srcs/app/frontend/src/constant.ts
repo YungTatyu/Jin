0bdca3eaee0f2c7143e5a6dca4ed291004de715e
rikeda-cloud <longsichitian648@gmail.com>
2024-10-05 00:02:00 +0900

    [replace] PROGRAM_ID

--- srcs/app/frontend/src/constant.ts
+++ srcs/app/frontend/src/constant.ts
@@ -2,5 +2,5 @@ import { PublicKey } from '@solana/web3.js';
 
 export const SOLANA_NETWORK = 'https://api.devnet.solana.com';
 export const PROGRAM_ID = new PublicKey(
-  'AbXkyPEuU7F231jG3Hew5zRdddKUejhMaWszdPh1r8uU'
+  '76HmiSofRjkEdLLmdiAx7LcZQyx5cgn6DaSQLzhMKAor'
 );
